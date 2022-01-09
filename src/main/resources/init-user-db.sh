3eba07075a51b589bd9f1ffe898c905b6b7a434a
Marcos Lozina <marcoslozina@gmail.com>
2022-01-09 11:30:17 -0300

    Delete old file
    delete file with corrupted format

--- src/main/resources/init-user-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER marcos WITH PASSWORD 'secretpassword';
-    CREATE DATABASE sumdb;
-    GRANT ALL PRIVILEGES ON DATABASE sumdb TO marcos;
-EOSQL
